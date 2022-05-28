345f68a14c3053634d717e377cecdefe749a4830
Valio <valentin.gaidarov@gmail.com>
2022-05-28 12:58:00 +0300

    add

---
+++ 01.USDtoBGN/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Converter_USD_BGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value in USD: ");
+            double usd = double.Parse(Console.ReadLine());
+            double bgn = usd * 1.79549;
+            Console.Write("The value in BGN is: ");
+            Console.WriteLine(Math.Round(bgn, 2));
+        }
+    }
+}
