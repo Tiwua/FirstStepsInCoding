8dc5131cb5fc1dff2a11ea717dda66bc6dc528f5
Valio <valentin.gaidarov@gmail.com>
2022-05-28 12:20:00 +0300

    added

--- 01.USDtoBGN/Program.cs
+++ 01.USDtoBGN/Program.cs
@@ -1,26 +1,16 @@
 ﻿using System;
 
-namespace _01.USDtoBGN
+namespace Converter_USD_BGN
 {
     class Program
     {
         static void Main(string[] args)
-
         {
-            int startingNumber = int.Parse(Console.ReadLine());
-            int finalNumber = int.Parse(Console.ReadLine());
-            int magicNumber = int.Parse(Console.ReadLine());
-            int combination = 0;
-            bool isFOund = false;
-
-            for (int firstNumber  = startingNumber; firstNumber <= finalNumber; finalNumber++)
-            {
-                for (int secondNumber  = startingNumber; secondNumber <= finalNumber; secondNumber++)
-                {
-
-                }
-            }
-
+            Console.Write("Enter value in USD: ");
+            double usd = double.Parse(Console.ReadLine());
+            double bgn = usd * 1.79549;
+            Console.Write("The value in BGN is: ");
+            Console.WriteLine(Math.Round(bgn, 2));
         }
     }
 }
